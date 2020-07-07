4bd6af092d3548c34fe23ede757fe30ca454a928
Noel S Cruz <65864827+noey2020@users.noreply.github.com>
2020-07-06 22:57:32 -0700

    Add files via upload

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #ifndef _NSC_MAIN_H
 #define _NSC_MAIN_H
+#define GPIO_AF2            0x2
 
 #include "stm32l1xx.h"
 #include "nc_stm32l1_rcc.h"
@@ -7,7 +8,7 @@
 #include "nc_defines.h"
 
 volatile uint32_t DEBUG_VAR = 0;      /* Declare global variables(outside main) "volatile" to force compiler to generate
-                                         code that rereads variable stored in memory & not just in registers. */
+                                         code that rereads variables stored in memory & not just in registers. */
 volatile uint8_t BACKGROUND = 0;
 volatile uint32_t SYSTICK = 0;
 volatile uint32_t msTicks = 0;        /* Variable to store millisecond ticks */
@@ -29,21 +30,21 @@ void SysTick_Handler(void){           /* SysTick interrupt Handler. */
 
 void init_ADC(void)
 {
-		/* We want to check if 01(0x1): HSI oscillator is used as system clock by checking RCC->CFGR SWS Flag. 
+		/* We want to check if 01(0x1): HSI oscillator is used as system clock by checking RCC->CFGR SWS Flag.
 		   Bits 3:2 SWS[1:0]: System clock switch status is checked by masking(bitwise AND)	first RCC->CFGR with 0b1100(0xC)
    		 and then checking for equality with 0b0100(0x4) */
 	  while((RCC->CFGR & 0xC) == RCC_CFGR_SWS_HSI){
 			  DEBUG_VAR = 0xFF88EEDD;         /* Hardware latency until it finally indicates SWS status as HSI oscillator used. */
 		}
-		
+
 		RCC->CR |= (1UL << 0);              /* Set Bit 0 HSION: Internal high-speed clock enable */
 		while((RCC->CR & RCC_CR_HSIRDY) == RCC_CR_HSIRDY){  /* Check RCC_CR_HSIRDY flag Bit 1 */
 		}
     RCC->CFGR |= (0UL << 11);          /* Clock configuration register (RCC_CFGR). Clear Bits 13:11 0xx: HCLK not divided PPRE2[2:0]: APB high-speed prescaler (APB2) */
-		
+
 		/* Enable ADC1 clock */
     //RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC1, ENABLE);
-	  RCC->APB2ENR |= (1UL << 9);        // Set bit 9 to enable ADC1 peripheral clock 
+	  RCC->APB2ENR |= (1UL << 9);        // Set bit 9 to enable ADC1 peripheral clock
 
 //    ADC_DeInit(ADC1);                           /* Put everything back to power-on defaults */
 
@@ -53,14 +54,14 @@ void init_ADC(void)
     //ADC1->CR2 |= ADC_CR2_CONT;                  /* Set bit 1 to enable continuous conversion. Manual configure but auto default in ADC_StructInit */
  //   ADC_Init(ADC1, &ADC_InitStructure);         /* Initializes the ADCx peripheral according to the specified parameters in the ADC_InitStruct. */
 
-		
+
     ADC1->CR2 &= ~ADC_CR2_ADON;        /* Clear Bit 0 ADON: A/D Converter ON / OFF. Or ADC1->CR2 |= (0UL << 0); */
 		ADC1->CR2 |= (0UL << 2);           /* Clear Bit 2 ADC_CFG: ADC configuration. 0: Bank A selected for channels ADC_IN0..31. line 1198 ADC_CR2_CFG stm32l1xx.h */
-		
+
 		ADC1->CR2 |= ADC_CR2_CONT;         /* Enable continuous conversiont */
 		ADC1->CR2 &= ~ADC_CR2_EXTSEL;      /* Clear Bits 27:24 EXTSEL[3:0]: External event select for regular group */
 		ADC1->CR2 |= (4UL << 24);          /* Select 0b0100(0x4): TIM3_TRGO event ADC trigger. Select 0b0111(0x7): TIM3_CC1 event. */
-		
+
 		/* Configure ADC regular sequence. 1) ADC regular sequence register 1 (ADC_SQR1). Bits 24:20 L[4:0]: Regular channel sequence length. These
        bits are written by software to define the total number of conversions in the regular channel conversion sequence.
        00000: 1 conversion
@@ -75,28 +76,35 @@ void init_ADC(void)
     ADC1->SQR5 = 0;                    /* Clear register. */
     //ADC1->SQR5 |= 10;
     ADC1->SQR5 |= (1UL << 4);          /* Place channel 1 in regular sequence. 0x10 = 0b10000 */
-		NVIC_SetPriority(ADC1_IRQn, 0x03); /* Set ADC1 priority 3(low priority) */
-		NVIC_EnableIRQ(ADC1_IRQn);         /* Enable ADC1 interrupt */		
+//		NVIC_SetPriority(ADC1_IRQn, 0x03); /* Set ADC1 priority 3(low priority) */
+//		NVIC_EnableIRQ(ADC1_IRQn);         /* Enable ADC1 interrupt */
 		ADC1->CR2 |= (1UL << 0);           /* Enable ADC. Set Bit 0. Or ADC1->CR2 &= ~ADC_CR2_ADON */
 		while((ADC1->SR & 0x40) == ADC_CR2_ADON){  /* ADC1->SR Bit 6 ADONS: ADC ON status. mask 0b1000000(0x40) */
 		    DEBUG_VAR = 0xBEEF012D3;       /* ADC not enabled yet so not ready to convert via ADONS ADC1->SR Bit 6 flag */
 		}
 		ADC1->SMPR3 |= (7UL << 0);         /* Configure sample time for channel 1 ADC_IN1 , slowest or max cycles to be safe */
-		ADC1->CR2 |= ADC_CR2_SWSTART;      /* Start conversion of regular channel */		
+		ADC1->CR2 |= ADC_CR2_SWSTART;      /* Start conversion of regular channel */
 }
 
 void init_TIM3(){
 	  /* Configure channel 1 output of timer 3 used as trigger signal of ADC. Square wave output with frequency 1Hz and duty cycle 50% */
 	  RCC->APB1ENR |= RCC_APB1ENR_TIM3EN; /* Enable TIM3 clock */
-		TIM3->PSC = 2097000/1000 - 1;     /* 16MHz is default internal clock. Prescaler value */
-		TIM3->ARR = 1000-1;               /* Auto-reload value */
+	  /* Output Compare Mode. Timer 3 channel 1 output 1Hz. page 395, 402 rm0038.
+	     1) Select counter clock(internal, external, and prescaler. Default: SMS=000 TIMx_SMCR register.  */
+		TIM3->PSC = 2097000/1000 - 1;     /* MSI 2.097MHz is default internal clock. Timer 3 counter clock 1KHz, Prescaler value */
+		/* 2) Write desired data in TIMx_ARR and TIMx_CCRx registers. */
+	  TIM3->ARR = 1000-1;               /* Auto-reload value */
     TIM3->CCR1 = 499;                 /* Output compare register for channel 1 */
-		TIM3->CCMR1 |= TIM_CCMR1_OC1M_1 | TIM_CCMR1_OC1M_2 | TIM_CCMR1_OC1M_0;   /* OC1M = 110 for PWM output mode 1 on channel 1 */
+	  /* 3) Set the CCxIE and/or CCxDE bits if an interrupt and/or a DMA request is to be generated. */
+	  /* 4) Select output mode */
+	  TIM3->CCMR1 |= TIM_CCMR1_OC1M_1 | TIM_CCMR1_OC1M_2 | TIM_CCMR1_OC1M_0;   /* OC1M = 110 for PWM output mode 1 on channel 1 */
 		TIM3->CCMR1	|= TIM_CCMR1_OC1PE;   /* Enable preload for channel 1 */
 		TIM3->CR1 |= TIM_CR1_ARPE;        /* Auto-reload preload enable */
-	  TIM3->CCER |= TIM_CCER_CC1E;      /* Capture/Compare 1 output enable */
-  	TIM3->EGR |= TIM_EGR_UG;          /* Update Generation, re-initialize timer counter */
-		TIM3->CR1 |= TIM_CR1_CEN;         /* Enable timer 3/counter */
+	  /* 5) Enable Output Enable Circuit, TIMx->CCER CC1E */
+  	TIM3->CCER |= TIM_CCER_CC1E;      /* Capture/Compare 1 output enable */
+	  TIM3->EGR |= TIM_EGR_UG;          /* Update Generation, re-initialize timer counter */
+		/* 6) Enable the counter by setting the CEN bit in the TIMx_CR1 register. */
+	  TIM3->CR1 |= TIM_CR1_CEN;         /* Enable timer 3/counter */
 }
 
 void ADC1_IRQHandler(void)
@@ -118,7 +126,7 @@ void ADC1_IRQHandler(void)
 
     BACKGROUND = 0;                   /* Clear all the toggle bits */
     SYSTICK = 0;
-  
+
 	  for (i = 0;i < 0x1000;i++)
 	  {
 		    ADC_VAR = 1;                  /* Set the ADC  toggle bit */
@@ -128,17 +136,36 @@ void ADC1_IRQHandler(void)
 		TIM3_VAR = 1;
 }
 
+void GPIO_Pin_Init(){
+		/* MODER6[1:0] involving bits 12 & 13. 0b0010(0x02): Alternate function mode */
+		RCC->AHBENR |= RCC_AHBENR_GPIOBEN;
+	GPIOB->MODER &= ~(0x03 << 12);			// Clear mode bits
+		GPIOB->MODER |= 0x02 << 12;					// Set pin PB.6 as alternate function
+		GPIOB->AFR[0] &= ~0xF << (24);			// Clear bits 24-27
+		GPIOB->AFR[0] |= GPIO_AF2 << (24);	// Set 0x02 mask alternate function
+		
+		GPIOB->OTYPER &= ~(0x1<<6);					// Set PB.6 pin as push-pull output type		
+		
+		// Set IO output speed
+		GPIOB->OSPEEDR &= ~(0x03<<(2*6));
+		GPIOB->OSPEEDR |= 0x03<<(2*6);
+	
+		// Set IO no pull-up pull-down
+		GPIOB->PUPDR |= ~(0x00<<(2*6));	
+}
+
 int main(void){
     uint32_t returnCode;
 
-    returnCode = SysTick_Config(SystemCoreClock / 1000);   
+    returnCode = 1; //SysTick_Config(SystemCoreClock / 1000);
 
-    if(returnCode != 0){                               
+    if(returnCode != 0){
     // Error Handling
     }
+		GPIO_Pin_Init();
 		init_TIM3();
-		init_ADC();
-		
+	//	init_ADC();
+
     while(1){
 			BACKGROUND = 1;
 		}
